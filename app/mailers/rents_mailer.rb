9e538faadaf94f08da558a570cdb8aa2633ed380
mpuricawolox <maykol.purica@wolox.cl>
2018-09-11 17:59:25 -0300

    Fixing style rules ofensses

--- app/mailers/rents_mailer.rb
+++ app/mailers/rents_mailer.rb
@@ -1,8 +1,7 @@
 class RentsMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
-  #bundle exec sidekiq --environment development -C config/sidekiq.yml
+  # bundle exec sidekiq --environment development -C config/sidekiq.yml
   #   en.rents_mailer.new_rent_notification.subject
   #
   def new_rent_notification(rent_id)
