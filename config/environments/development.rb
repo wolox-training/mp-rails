6d16776c14512d81cfd178bb4fc94d41c8c55251
mpuricawolox <maykol.purica@wolox.cl>
2018-09-11 17:51:41 -0300

    Adding settings for sidekiq

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
   
@@ -68,4 +68,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.active_job.queue_adapter = :sidekiq
 end
