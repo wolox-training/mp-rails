fdcb05137c60eeaf2f8a5e40d470cf7a0a62dc34
mpuricawolox <maykol.purica@wolox.cl>
2018-09-20 10:11:23 -0300

    Deleting comments

--- app/services/open_library_service/valid_isbn.rb
+++ app/services/open_library_service/valid_isbn.rb
@@ -1,15 +1,9 @@
-# ^(?:ISBN(?:-1[03])?:?●)?(?=[-0-9●]{17}$|[-0-9X●]{13}$|[0-9X]{10}$)↵
-# (?:97[89][-●]?)?[0-9]{1,5}[-●]?(?:[0-9]+[-●]?){2}[0-9X]$
 require 'library_stdnums'
 module OpenLibraryService
   module ValidIsbn
     include StdNum
 
     def self.valid_isbn?(isbn)
-      # pattern = Regexp.new('^(?:ISBN(?:-1[03])?:?●)?(?=[-0-9●]{17}$|[-0-9X●]{13}$|[0-9X]{10}$)↵
-      # (?:97[89][-●]?)?[0-9]{1,5}[-●]?(?:[0-9]+[-●]?){2}[0-9X]$')
-      # #pattern.match?(isbn)
-
       ISBN.valid?(isbn)
     end
   end
