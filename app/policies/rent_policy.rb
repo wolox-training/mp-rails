22ca28e9a7df9e28eb6d3e022f4a47214f65b8f9
mpuricawolox <maykol.purica@wolox.cl>
2018-09-20 11:23:28 -0300

    Deleting comments

--- app/policies/rent_policy.rb
+++ app/policies/rent_policy.rb
@@ -1,26 +1,3 @@
-# class RentPolicy
-#   class Scope
-#     attr_reader :user, :scope
-
-#     def initialize(user, scope)
-#       @user = user
-#       @scope = scope
-#     end
-
-#     def resolve
-#       scope.where(user: user)
-#     end
-
-#     def show?
-#       true
-#     end
-
-#     def index?
-#       true
-#     end
-#   end
-# end
-
 class RentPolicy
   attr_reader :user, :record
 
